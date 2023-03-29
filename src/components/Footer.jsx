f8088ad7a367c3376bfe86d39de3e91c78252e50
Md Al Imran Adoy <imranadoy@gmail.com>
2023-03-29 18:00:50 +0600

    Added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="footer-content">
+          <div className="footer-content__socials">
+          <a href="https://adoydevs.github.io/"><i className="fa-brands fa-twitter"></i></a>
+          <a href="https://adoydevs.github.io/"><i className="fa-brands fa-discord"></i></a>
+          <a href="https://adoydevs.github.io/"><i className="fa-brands fa-facebook"></i></a>
+          <a href="https://adoydevs.github.io/"><i className="fa-brands fa-youtube"></i></a>
+          </div>
+          <div className="footer-content__text">
+            <p>Privacy</p>
+            <p>Terms of Use</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
